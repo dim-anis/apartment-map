6c383a0bbdb3c1298f3576dece70336fe3d0f714
dim-anis <anis.dim@gmail.com>
2024-08-21 14:34:46 +0700

    give up on trying to deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/apartment-map/",
   plugins: [
     remix({
-      basename: "/apartment-map/",
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
